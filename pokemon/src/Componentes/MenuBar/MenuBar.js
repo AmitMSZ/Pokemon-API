f24b93e11ba9df5217cd72233391678264a80b4e
Amit Martinez <amitmartinez50@gmail,com>
2022-07-22 10:51:40 -0400

    Integracion de un MenuBar

---
+++ pokemon/src/Componentes/MenuBar/MenuBar.js
@@ -0,0 +1,113 @@
+import React from "react";
+import {AppBar, Button, IconButton, Menu, MenuItem, Toolbar, Typography} from "@mui/material"
+import { Box, Container } from "@mui/system";
+import MenuIcon from '@mui/icons-material/Menu';
+import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
+
+const pages = ['Historial', 'Combate', "Lista de Pokemón's"];
+const MenuBar = (props) => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+    return ( 
+        <AppBar color='default' position="static">
+            <Container maxWidth='x1'>
+                <Toolbar disableGutters>
+                    <CatchingPokemonIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component='a'
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Pokemón
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton 
+                        size='large'
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        color="inherit"
+                        onClick={handleOpenNavMenu}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                            display: { xs: 'block', md: 'none' },
+                        }}
+                        >
+                            {pages.map((page) => (
+                            <MenuItem key={page} onClick={handleCloseNavMenu}>
+                            <Typography textAlign="center">{page}</Typography>
+                            </MenuItem>
+                        ))}
+                        </Menu>
+                    </Box>
+
+
+                    <CatchingPokemonIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component='a'
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Pokemón
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                            ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+
+export default MenuBar ;
